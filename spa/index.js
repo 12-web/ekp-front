866dee564dccd900aa7dcf51a45f6d4dc44a1579
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2025-11-12 22:48:59 +0300

    added spa events

--- spa/index.js
+++ spa/index.js
@@ -1,46 +1,138 @@
 // пример отправки события
 
-(function () {
-    window.gpnAnalytics &&
-        window.gpnAnalytics.sendEvent(3, {
-            componentId: "user_login",
-            constaeventtype: "undefined",
-            component: "OTHER",
-            customparams: [{ as_user_id: true, value: "1" }],
-        });
-})();
+// (function () {
+//     window.gpnAnalytics &&
+//         window.gpnAnalytics.sendEvent(3, {
+//             componentId: "user_login",
+//             constaeventtype: "undefined",
+//             component: "OTHER",
+//             customparams: [{ as_user_id: true, value: "1" }],
+//         });
+// })();
 
 //Новости
 
 (function () {
-    // нажатие на кнопки переключения новостей
-    const arrowBtnContainer = document.querySelector(".arrow-btn-container");
-
-    const handleArrowClick = (e) => {
-        const btn = e.target.closest("button");
-
-        if (!e || !btn) return;
+    // загрузка страницы новости
+    const onLoadNews = () => {
+        const customparams = [
+            {
+                user_id: "",
+                user_do: "",
+                news_title: "",
+                news_id: "",
+                news_tag: "",
+                content_type: "",
+                type_new: "",
+                publishdate: "",
+            },
+        ];
 
         window.gpnAnalytics &&
             window.gpnAnalytics.sendEvent(3, {
-                componentId: "interaction_with_the_new",
+                componentId: "open_new",
+                constaeventtype: "undefined",
+                component: "OTHER",
+                customparams,
+            });
+    };
+
+    window.addEventListener("load", onLoadNews);
+
+    // нажатие на кнопку лайка
+    const likeBtn = document.querySelector(".news-template-assets_likes");
+
+    const handleArrowClick = () => {
+        if (!likeBtn) return;
+
+        const customparams = [
+            {
+                user_id: "",
+                user_do: "",
+                news_title: "",
+                news_id: "+",
+                news_tag: "+",
+                content_type: "",
+                type_new: "",
+                type_like: "",
+                publishdate: "+",
+            },
+        ];
+
+        window.gpnAnalytics &&
+            window.gpnAnalytics.sendEvent(3, {
+                componentId: "news_like_pressed",
+                constaeventtype: "undefined",
+                component: "OTHER",
+                customparams,
+            });
+    };
+
+    likeBtn?.addEventListener("click", handleArrowClick);
+
+    //Нажатие на теги новостей
+    const tagsContainer = document.querySelector(".news-template-tags-items");
+
+    const handleTagsClick = (e) => {
+        const tag = e?.target.closest(".news-template-tags-item");
+
+        if (!tag) return;
+
+        window.gpnAnalytics &&
+            window.gpnAnalytics.sendEvent(3, {
+                componentId: "news_tag_pressed",
                 constaeventtype: "undefined",
                 component: "OTHER",
                 customparams: [
                     {
                         user_id: "",
                         user_do: "",
-                        news_title: "",
-                        news_id: "",
-                        news_tag: "",
-                        content_type: "",
-                        type_new: "",
-                        publishdate: "",
-                        interaction: "",
+                        tag: "",
+                        page: "",
+                        location_tag: "",
+                        action_tag: "",
                     },
                 ],
             });
     };
 
-    arrowBtnContainer?.addEventListener("click", handleArrowClick);
+    tagsContainer?.addEventListener("click", handleTagsClick);
+
+    //Скролл до конца новости
+    const depthValue = 0.9;
+    let depthGoal = true;
+
+    const handleScrollEnd = () => {
+        if (!depthGoal) return;
+
+        const isCompleteDepth =
+            document.documentElement.scrollHeight -
+                document.documentElement.scrollTop -
+                document.documentElement.clientHeight <
+            document.documentElement.scrollHeight * (1 - depthValue);
+
+        if (isCompleteDepth) {
+            window.gpnAnalytics &&
+                window.gpnAnalytics.sendEvent(3, {
+                    componentId: "finish_new",
+                    constaeventtype: "undefined",
+                    component: "OTHER",
+                    customparams: [
+                        {
+                            user_id: "",
+                            user_do: "",
+                            news_title: "",
+                            news_id: "",
+                            news_tag: "",
+                            content_type: "",
+                            type_new: "",
+                            publishdate: "",
+                        },
+                    ],
+                });
+            depthGoal = false;
+        }
+    };
+
+    window.addEventListener("scroll", handleScrollEnd);
 })();
--- spa/script.ftl
+++ spa/script.ftl
@@ -216,4 +216,129 @@
 
 	const initMediagalleryEvent = new CustomEvent('initMediagallery');
 	document.dispatchEvent(initMediagalleryEvent);
+
+  (function () {
+    // загрузка страницы новости
+    const onLoadNews = () => {
+        const customparams = [
+            {
+                user_id: "",
+                user_do: "",
+                news_title: "",
+                news_id: ${journalArticleId},
+                news_tag: ${tags?join(", ")},
+                content_type: "",
+                type_new: "",
+                publishdate: ${publishDate},
+            },
+        ];
+
+        window.gpnAnalytics &&
+            window.gpnAnalytics.sendEvent(3, {
+                componentId: "open_new",
+                constaeventtype: "undefined",
+                component: "OTHER",
+                customparams,
+            });
+    };
+
+    window.addEventListener("load", onLoadNews);
+
+    // Нажатие на кнопку лайка
+    const likeBtn = document.querySelector(".news-template-assets_likes");
+
+    const handleLikeClick = () => {
+        if (!likeBtn) return;
+
+        const customparams = [
+                    {
+                        user_id: "",
+                        user_do: "",
+                        news_title: "",
+                        news_id: ${journalArticleId},
+                        news_tag: ${tags?join(", ")},
+                        content_type: "",
+                        type_new: "",
+                        type_like: "",
+                        publishdate: ${publishDate},
+                    },
+                ];
+
+        window.gpnAnalytics &&
+            window.gpnAnalytics.sendEvent(3, {
+                componentId: "news_like_pressed",
+                constaeventtype: "undefined",
+                component: "OTHER",
+                customparams,
+            });
+    };
+
+    likeBtn?.addEventListener("click", handleLikeClick);
+
+    //Нажатие на теги новостей
+    const tagsContainer = document.querySelector(".news-template-tags-items");
+
+    const handleTagsClick = (e) => {
+        const tag = e?.target.closest(".news-template-tags-item");
+
+        if (!tag) return;
+
+        window.gpnAnalytics &&
+            window.gpnAnalytics.sendEvent(3, {
+                componentId: "news_tag_pressed",
+                constaeventtype: "undefined",
+                component: "OTHER",
+                customparams: [
+                    {
+                        user_id: "",
+                        user_do: "",
+                        tag: tag.textContent,
+                        page: "",
+                        location_tag: "внутри новости",
+                        action_tag: "переход",
+                    },
+                ],
+            });
+    };
+
+    tagsContainer?.addEventListener("click", handleTagsClick);
+
+    //Скролл до конца новости
+    const depthValue = 0.9;
+    let depthGoal = true;
+
+    const handleScrollEnd = () => {
+        if (!depthGoal) return;
+
+        const isCompleteDepth =
+            document.documentElement.scrollHeight -
+                document.documentElement.scrollTop -
+                document.documentElement.clientHeight <
+            document.documentElement.scrollHeight * (1 - depthValue);
+
+        if (isCompleteDepth) {
+            window.gpnAnalytics &&
+                window.gpnAnalytics.sendEvent(3, {
+                    componentId: "finish_new",
+                    constaeventtype: "undefined",
+                    component: "OTHER",
+                    customparams: [
+                        {
+                            user_id: "",
+                            user_do: "",
+                            news_title: "",
+                            news_id: ${journalArticleId},
+                            news_tag: ${tags?join(", ")},
+                            content_type: "",
+                            type_new: "",
+                            publishdate: ${publishDate},
+                        },
+                    ],
+                });
+            depthGoal = false;
+        }
+    };
+
+    window.addEventListener("scroll", handleScrollEnd);
+})();
 </script>
